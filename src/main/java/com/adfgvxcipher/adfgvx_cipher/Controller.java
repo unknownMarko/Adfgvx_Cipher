34e6339d38b1e7c516888f54b715dea6a02201ac
unknownMarko <marko.adamek@gmail.com>
2024-10-21 00:12:17 +0200

    Refactoring

--- src/main/java/com/adfgvxcipher/adfgvx_cipher/Controller.java
+++ src/main/java/com/adfgvxcipher/adfgvx_cipher/Controller.java
@@ -3,7 +3,7 @@ package com.adfgvxcipher.adfgvx_cipher;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
 
-public class HelloController {
+public class Controller {
     @FXML
     private Label welcomeText;
 
