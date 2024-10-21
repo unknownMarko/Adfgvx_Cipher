0e2789596d6d042381ed5a99d7e95c9c9cf60d5d
unknownMarko <marko.adamek@gmail.com>
2024-10-21 12:23:45 +0200

    Controller connected to adfgvx_cipher_frontend.fxml

--- src/main/java/com/adfgvxcipher/adfgvx_cipher/Controller.java
+++ src/main/java/com/adfgvxcipher/adfgvx_cipher/Controller.java
@@ -1,14 +1,163 @@
 package com.adfgvxcipher.adfgvx_cipher;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.scene.input.KeyEvent;
+import javafx.scene.layout.GridPane;
 
 public class Controller {
-    @FXML
-    private Label welcomeText;
 
     @FXML
-    protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome to JavaFX Application!");
+    private Button btn_decrypt;
+
+    @FXML
+    private Button btn_encrypt;
+
+    @FXML
+    private TextField field_alphabet;
+
+    @FXML
+    private TextField field_input;
+
+    @FXML
+    private TextField field_input_filtered;
+
+    @FXML
+    private TextField field_key;
+
+    @FXML
+    private TextField field_key_filtered;
+
+    @FXML
+    private TextField field_output;
+
+    @FXML
+    private GridPane grid;
+
+    @FXML
+    private Label gridLabel06;
+
+    @FXML
+    private Label gridLabel07;
+
+    @FXML
+    private Label gridLabel08;
+
+    @FXML
+    private Label gridLabel09;
+
+    @FXML
+    private Label gridLabel1;
+
+    @FXML
+    private Label gridLabel10;
+
+    @FXML
+    private Label gridLabel11;
+
+    @FXML
+    private Label gridLabel12;
+
+    @FXML
+    private Label gridLabel13;
+
+    @FXML
+    private Label gridLabel14;
+
+    @FXML
+    private Label gridLabel15;
+
+    @FXML
+    private Label gridLabel16;
+
+    @FXML
+    private Label gridLabel17;
+
+    @FXML
+    private Label gridLabel18;
+
+    @FXML
+    private Label gridLabel19;
+
+    @FXML
+    private Label gridLabel2;
+
+    @FXML
+    private Label gridLabel20;
+
+    @FXML
+    private Label gridLabel21;
+
+    @FXML
+    private Label gridLabel22;
+
+    @FXML
+    private Label gridLabel23;
+
+    @FXML
+    private Label gridLabel24;
+
+    @FXML
+    private Label gridLabel25;
+
+    @FXML
+    private Label gridLabel26;
+
+    @FXML
+    private Label gridLabel27;
+
+    @FXML
+    private Label gridLabel28;
+
+    @FXML
+    private Label gridLabel29;
+
+    @FXML
+    private Label gridLabel3;
+
+    @FXML
+    private Label gridLabel30;
+
+    @FXML
+    private Label gridLabel31;
+
+    @FXML
+    private Label gridLabel32;
+
+    @FXML
+    private Label gridLabel33;
+
+    @FXML
+    private Label gridLabel34;
+
+    @FXML
+    private Label gridLabel35;
+
+    @FXML
+    private Label gridLabel36;
+
+    @FXML
+    private Label gridLabel4;
+
+    @FXML
+    private Label gridLabel5;
+
+    @FXML
+    void handleDecryptButton(KeyEvent event) {
+
     }
+
+    @FXML
+    void handleEncryptButton(ActionEvent event) {
+
+    }
+
+    @FXML
+    void handleInput(KeyEvent event) {
+
+    }
+
 }
